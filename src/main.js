9ebbf79a08dd36eb575ef4056fa25afb89351bec
tiago <tiagobrunobfr@gmail.com>
2020-06-12 14:31:17 -0300

    config mock api finished

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
-
+import { makeServer } from './api/server'
 Vue.config.productionTip = false
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
 
 new Vue({
   router,
